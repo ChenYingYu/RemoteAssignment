41db949b7999953b5ac63692c512b3cb95ad03b9
ChenYingYu <chenalan@ChenAlandeMacBook-Air.local>
2018-02-13 20:38:56 +0800

    Finish Objective-Oriented Swift.playground

--- week2/HW_Objective-Oriented Swift.playground/Contents.swift
+++ week2/HW_Objective-Oriented Swift.playground/Contents.swift
@@ -1,1 +1,77 @@
+//  HW_Objective-Oriented swift
+//
+//  Created by ChenAlan on 2018/2/12.
+//
+//  Remote Assignment - Week 2
+/*
+ ----------------
+ Object-Oriented
+ ----------------
+ */
 
+import Foundation
+
+/*
+ Q1: Declare a class
+ */
+enum Gender {
+    case male
+    case female
+    case undefined
+}
+
+class Animal {
+    // Not sure let or var
+    let gender: Gender
+    func eat() {
+        print("I eat everything!")
+    }
+    
+    init(gender: Gender) {
+        self.gender = gender
+    }
+}
+
+/*
+ Q2: Class inheritance
+ */
+class Tiger: Animal {
+    override func eat() {
+        print("I eat meat!")
+    }
+}
+
+class Horse: Animal {
+    override func eat() {
+        print("I eat carrots!")
+    }
+}
+
+class Elephant: Animal {
+    override func eat() {
+        print("I eat plants!")
+    }
+}
+
+
+/*
+ Q3:
+ */
+class Zoo {
+    var weeklyHot: Animal?
+//    init(animal: Animal?) {
+//        self.weeklyHot = animal
+//    }
+}
+
+let zoo = Zoo()
+let tiger = Tiger(gender: Gender.male)
+let elephant = Elephant(gender: Gender.female)
+let horse = Horse(gender: Gender.undefined)
+
+zoo.weeklyHot = tiger
+print(zoo.weeklyHot!)
+zoo.weeklyHot = elephant
+print(zoo.weeklyHot!)
+zoo.weeklyHot = horse
+print(zoo.weeklyHot!)
