e47227971210c4c1f6d0c1bfb68e761960c16b62
ChenYingYu <chenalan@ChenAlandeMacBook-Air.local>
2018-02-14 12:24:40 +0800

    Create HW_Protocol_in_Swift.playground

---
+++ week2/HW_Protocol_in_Swift.playground/Contents.swift
@@ -0,0 +1,27 @@
+//  HW_Protocol_in_Swift
+//
+//  Created by ChenAlan on 2018/2/12.
+//
+//  Remote Assignment - Week 2
+/*
+ ------------------
+ Protocol in Swift
+ ------------------
+ */
+
+import Foundation
+
+/*
+ Q1:
+ */
+
+
+/*
+ Q2:
+ */
+
+
+/*
+ Q3:
+ */
+
