32f6294a7ae2b45eba809b6f2a857049477ec480
ChenYingYu <chenalan@ChenAlandeMacBook-Air.local>
2018-02-25 16:58:26 +0800

    Create basic UI: hintLabel, guessLabel, buttons

--- week3/終極密碼/終極密碼/ViewController.swift
+++ week3/終極密碼/終極密碼/ViewController.swift
@@ -9,17 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
-
-
+    @IBOutlet weak var hintLabel: UILabel!
+    @IBOutlet weak var guessLabel: UILabel!
+    @IBOutlet var buttons: [UIButton]! {
+        didSet {
+            updateViewFromModel()
+        }
+    }
+    
+    func updateViewFromModel() {
+        for index in buttons.indices {
+            let button = buttons[index]
+            button.layer.borderWidth = 2.0
+            button.layer.borderColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+            button.layer.cornerRadius = button.frame.size.height / 2
+        }
+    }
+    
 }
 
