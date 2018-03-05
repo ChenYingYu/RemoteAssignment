28c2e409ae444f11c41dd18e229ff93e598ff81f
ChenYingYu <chenalan@ChenAlandeMacBook-Air.local>
2018-03-05 10:40:35 +0800

    Add ASiC_YouBike

---
+++ week4/ASiC_YouBike/ASiC_YouBike/Controller/YouBikeViewController.swift
@@ -0,0 +1,33 @@
+//
+//  YouBikeViewController.swift
+//  ASiC_YouBike
+//
+//  Created by WU CHIH WEI on 2018/2/25.
+//  Copyright © 2018年 WU CHIH WEI. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+protocol StationInfo {
+    
+    var name: String { get }
+    
+    var position: String { get }
+    
+    var remainBikes: String { get }
+}
+
+class YouBikeViewController: UIViewController {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    let closureClient = ClosureClient()
+    
+    var delegateClient = DelegateClient()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
